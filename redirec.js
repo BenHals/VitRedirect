a45368b51efd9816cba1b62367da264569a80f4f
Ben Halstead <Ben Halstead>
2016-03-11 14:17:46 +1300

    remove alert

--- redirec.js
+++ redirec.js
@@ -1,4 +1,4 @@
-alert(window.location.search);
-test = "file:///C:/Users/Ben/Dropbox/Doc/Work/Stats%20Schol/redirect/redirec.html?url=http://new.censusatschool.org.nz/tools/random-sampler/tmp/98560258_110316_124957.csv&land=visualize";
+
+test = "benhals.github.io/VitRedirect/index.html?url=http://new.censusatschool.org.nz/tools/random-sampler/tmp/98560258_110316_124957.csv&land=visualize";
 var updateLink = "http://gqnoqjj3.docker.stat.auckland.ac.nz";
 window.location = updateLink + window.location.search;
